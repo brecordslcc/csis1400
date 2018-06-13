b638deade37aa70b3f4a8fe435f114c42909d737
Ben <benjamin.s.record@gmail.com>
2018-06-13 07:33:15 -0600

    Updating hw1

--- hw1/src/main/java/com/brecord/csis1400/hw1/Application.java
+++ hw1/src/main/java/com/brecord/csis1400/hw1/Application.java
@@ -1,5 +1,6 @@
 package com.brecord.csis1400.hw1;
 
+import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -10,29 +11,33 @@ public class Application {
         final String url = "http://images.clipartpanda.com/simple-black-and-white-tree-design-jixxLnAiE.png";
 //        final String url = "https://d2gg9evh47fn9z.cloudfront.net/800px_COLOURBOX19008563.jpg";
 
-        final BufferedImage img = ImageIO.read(new URL(url));
+        try {
+            final BufferedImage img = ImageIO.read(new URL(url));
 
-        final int height = img.getHeight(),
-                width = img.getWidth(),
-                maxWidth = 100,
-                maxHeight = 50;
+            final int height = img.getHeight(),
+                    width = img.getWidth(),
+                    maxWidth = 100,
+                    maxHeight = 50;
 
-        final double stepY = (double) height / (double) maxHeight,
-                stepX = (double) width / (double) maxWidth;
+            final double stepY = (double) height / (double) maxHeight,
+                    stepX = (double) width / (double) maxWidth;
 
-        final StringBuilder sb = new StringBuilder();
+            final StringBuilder sb = new StringBuilder();
 
-        for (int y = 0; y < height; y += stepY < 1 ? 1 : stepY) {
-            for (int x = 0; x < width; x += stepX < 1 ? 1 : stepX) {
-                final int rgb = img.getRGB(x, y);
-                int red = (rgb & 0x00ff0000) >> 16,
-                        green = (rgb & 0x0000ff00) >> 8,
-                        blue = rgb & 0x000000ff;
-                sb.append(red < 125 && green < 125 && blue < 125 ? '*' : ' ');
+            for (int y = 0; y < height; y += stepY < 1 ? 1 : stepY) {
+                for (int x = 0; x < width; x += stepX < 1 ? 1 : stepX) {
+                    final int rgb = img.getRGB(x, y);
+                    int red = (rgb & 0x00ff0000) >> 16,
+                            green = (rgb & 0x0000ff00) >> 8,
+                            blue = rgb & 0x000000ff;
+                    sb.append(red < 125 && green < 125 && blue < 125 ? '*' : ' ');
+                }
+                sb.append("\n");
             }
-            sb.append("\n");
-        }
 
-        System.out.println(sb.toString());
+            System.out.println(sb.toString());
+        } catch (final IIOException e) {
+            System.out.println("ERROR: Network connection to the internet is required for this application.");
+        }
     }
 }
