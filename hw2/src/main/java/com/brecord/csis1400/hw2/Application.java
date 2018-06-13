7185579777e9e425a718c007deb7487f96167f48
brecord <brecord2@bruinmail.slc.edu>
2018-06-13 10:19:15 -0600

    hw2

---
+++ hw2/src/main/java/com/brecord/csis1400/hw2/Application.java
@@ -0,0 +1,31 @@
+package com.brecord.csis1400.hw2;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) throws IOException {
+        final StringBuilder sb = new StringBuilder();
+        final List<String> titles = Arrays.asList("number", "square", "cube");
+        final byte columns = 3,
+                columnWidth = 8;
+
+        for (int i = -1; i <= 10; i++) {
+            sb.append("\n");
+            for (byte j = 0; j < columns; j++) {
+                final String val = String.valueOf((int) Math.pow(i, j + 1));
+                for (byte k = 0; k < columnWidth; k++) {
+                    if (i == -1)
+                        sb.append(titles.get(j).length() <= k ? " " : titles.get(j).charAt(k));
+                    else
+                        sb.append(val.length() <= k ? " " : val.charAt(k));
+                }
+            }
+        }
+
+        sb.append("\nPress any key to continue . . .");
+        System.out.println(sb.toString());
+        System.in.read();
+    }
+}
